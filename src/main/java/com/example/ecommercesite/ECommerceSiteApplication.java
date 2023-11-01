3d000b9638e82ca1150c34a4b63f58fe9df24b0f
HimaGirija Ponnapalli <girija.bio@gmail.com>
2023-11-01 08:18:29 +0530

    first commit

---
+++ src/main/java/com/example/ecommercesite/ECommerceSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecommercesite/ECommerceSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
